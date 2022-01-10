195509a7bdb9bd862c8d2c982c7184683f73c94f
Didin Yahya <didinnuryahya@gmail.com>
2022-01-10 10:45:53 +0700

    add function delete data

--- server/controllers/loanBook.js
+++ server/controllers/loanBook.js
@@ -28,6 +28,7 @@ exports.insert = async (req, res) => {
     response({
       res,
       message: "Successfully added a new book to the library",
+      statusCode: 200,
       data,
     });
   } catch (error0) {
@@ -40,22 +41,20 @@ exports.insert = async (req, res) => {
   }
 };
 
-
 exports.find = async (req, res) => {
-  try{
+  try {
     const { q, page, limit } = req.query;
     const paramExists = Object.keys(req.query).length > 0;
     let loans;
     let detailloans;
 
     if (!paramExists) {
-       loans = await LoanBookModel.findAll({
+      loans = await LoanBookModel.findAll({
         where: {
           loanCode: req.body.loanCode,
         },
         logging: false,
       });
-
 
       detailloans = await DetailLoanBookModel.findAll({
         where: {
@@ -67,13 +66,12 @@ exports.find = async (req, res) => {
 
     if (paramExists) {
       if (q.length === 0) {
-         loans = await LoanBookModel.findAll({
+        loans = await LoanBookModel.findAll({
           where: {
             loanCode: req.body.loanCode,
           },
           logging: false,
         });
-
 
         detailloans = await DetailLoanBookModel.findAll({
           where: {
@@ -82,7 +80,7 @@ exports.find = async (req, res) => {
           logging: false,
         });
       } else {
-          loans = await LoanBookModel.findAll({
+        loans = await LoanBookModel.findAll({
           where: {
             loanCode: req.body.loanCode,
           },
@@ -90,5 +88,63 @@ exports.find = async (req, res) => {
         });
       }
     }
+
+    const dataloans = pagination(loans, page, limit);
+    const datadetailloans = pagination(detailloans, page, limit);
+
+    var dataloansall = {
+      loans: dataloans,
+      detailloans: datadetailloans,
+    };
+    response({
+      res,
+      message: "Data Loans request allowed",
+      statusCode: 200,
+      data: paramExists ? data : dataloansall,
+    });
+  } catch (error0) {
+    response({
+      success: false,
+      res,
+      message: error0.message,
+      statusCode: 400,
+    });
   }
-}
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const dataloans = await LoanBookModel.destroy({
+      where: {
+        loanCode: req.body.loanCode,
+      },
+      logging: false,
+    });
+
+    const datadetailloans = await DetailLoanBookModel.destroy({
+      where: {
+        loanCode: req.body.loanCode,
+      },
+      logging: false,
+    });
+
+    var dataloansall = {
+      loans: dataloans,
+      detailloans: datadetailloans,
+    };
+
+    response({
+      res,
+      message: "Managed to delete the book",
+      statusCode: 200,
+      dataloansall,
+    });
+  } catch (error0) {
+    response({
+      success: false,
+      res,
+      message: error0.message,
+      statusCode: 400,
+    });
+  }
+};
